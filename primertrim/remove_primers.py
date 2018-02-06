4e1d16c37294ea592a655274abb4a6db3b1f4c01
Kyle Bittinger <kylebittinger@gmail.com>
2018-02-06 06:59:03 -0500

    Use FileType objects in argparse library

--- primertrim/remove_primers.py
+++ primertrim/remove_primers.py
@@ -24,53 +24,38 @@ def parse_fastq(f):
 def main(argv=None):
    p = argparse.ArgumentParser()
    p.add_argument("-r", "--read", help="remove_fwd_primer_from_R2 OR remove_rev_primer_from_R1")
-   p.add_argument("-i", "--in_fastq_FP", help="PATH to input fastq to be trimmed")
-   p.add_argument("-o", "--out_fastq_FP", help="PATH to output fastq after trimming")
+   p.add_argument(
+      "-i", "--in_fastq", type=argparse.FileType('r'),
+      help="PATH to input fastq to be trimmed")
+   p.add_argument(
+      "-o", "--out_fastq", type=argparse.FileType('w'),
+      help="PATH to output fastq after trimming")
+   p.add_argument(
+      "--log", type=argparse.FileType('w'),
+      help="Log file to record location of primers detected")
    args = p.parse_args(argv)
 
    which_primer = args.read
-   in_fastq_FP = args.in_fastq_FP
-   out_fastq_FP = args.out_fastq_FP
+   in_fastq = args.in_fastq
+   out_fastq = args.out_fastq
+   log = args.log
 
    if which_primer == "remove_fwd_primer_from_R2":
-      ### if input or output fastq file name ends with R1.fastq, then something is wrong
-      if args.in_fastq_FP.endswith("R1.fastq") or out_fastq_FP.endswith("R1.fastq"):
-         print("Error: R2 sequences expected in both input and output")
-         sys.exit(0)
-      elif in_fastq_FP.endswith("R2.fastq") and out_fastq_FP.endswith("R2.fastq"):
          primer = "TTACTTCCTCTAAATGACCAAG" ### rev complement of ITS1F primer	
-      else: 
-         print("Error: invalid input/output fastq file paths")
-         sys.exit(0)
    elif which_primer == "remove_rev_primer_from_R1":
-	### if input or output fastq file name ends with R2.fastq, then something is wrong
-      if in_fastq_FP.endswith("R2.fastq") or out_fastq_FP.endswith("R2.fastq"):
-         print("Error: R1 sequences expected in both input and output")
-         sys.exit(0)
-      elif in_fastq_FP.endswith("R1.fastq") and out_fastq_FP.endswith("R1.fastq"):
          primer = "GCATCGATGAAGAACGCAGC" ### rev complement of ITS2R primer    
-      else: 
-         print("Error: invalid input/output fastq file paths")
-         sys.exit(0)
-   else: 
-      print("Error: invalid argument.\nProvide either 'remove_fwd_primer_from_R2' or 'remove_rev_primer_from_R1'")
 
-   log_file_base = os.path.splitext(out_fastq_FP)[0]
-   log_file_FP = log_file_base + ".log" 
-   in_fastq = open(in_fastq_FP)
    reads = (FastqRead(x) for x in parse_fastq(in_fastq))
 
-   with open(out_fastq_FP, "w") as out, open(log_file_FP, "w") as log:
-      for read in reads:
-         if primer in read.seq:
-            primer_loc = read.seq.index(primer) ### returns first position of primer sequences match
-            trimmed_seq = read.seq[:primer_loc]
-            trimmed_qual = read.qual[:primer_loc]
-         else:
-            primer_loc = "NA"
-            trimmed_seq = read.seq
-            trimmed_qual = read.qual
-         out.write("@%s\n%s\n+\n%s\n" % (read.desc, trimmed_seq, trimmed_qual))
-         log.write("%s\t%s\n" % (read.desc, primer_loc))
-   in_fastq.close()
+   for read in reads:
+       if primer in read.seq:
+          primer_loc = read.seq.index(primer) ### returns first position of primer sequences match
+          trimmed_seq = read.seq[:primer_loc]
+          trimmed_qual = read.qual[:primer_loc]
+       else:
+          primer_loc = "NA"
+          trimmed_seq = read.seq
+          trimmed_qual = read.qual
+       out_fastq.write("@%s\n%s\n+\n%s\n" % (read.desc, trimmed_seq, trimmed_qual))
+       log.write("%s\t%s\n" % (read.desc, primer_loc))
 
