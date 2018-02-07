dd8d6c920bf83023b4ba7391c4c31990639d0c1a
Kyle Bittinger <kylebittinger@gmail.com>
2018-02-07 00:51:21 -0500

    Made Jung-Jin the author

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='primertrim',
     version='0.0.1',
     description='Trim primer sequences from FASTQ files',
-    author='Kyle Bittinger',
-    author_email='kylebittinger@gmail.com',
+    author='Jung-Jin Lee',
+    author_email='junglee0713@gmail.com',
     url='https://github.com/junglee0713/ITS_primer_trim',
     packages=['primertrim'],
     entry_points = {
